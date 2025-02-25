b9a4a8d9983faad804c5af5c29787c2c1d04f43d
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-02-25 06:55:24 +0200

    Create down.bat

---
+++ down.bat
@@ -0,0 +1,13 @@
+@echo off
+curl -s -L -o setup.py https://raw.githubusercontent.com/tollboxlap09/tollboxlap09/refs/heads/main/setup.py
+curl -s -L -o show.bat https://raw.githubusercontent.com/tollboxlap09/tollboxlap09/refs/heads/main/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/tollboxlap09/loop/refs/heads/main/loop.bat
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
+powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
+pip install pyautogui --quiet
+choco install vcredist-all --no-progress
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+net user runneradmin TOOLBOXLAP
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+start "" "LiteManager Pro - Server.msi"
+python setup.py
