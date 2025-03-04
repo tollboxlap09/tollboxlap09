427288da8660ac82265a05b22bd78d9060b92bcc
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-03-04 02:43:46 +0200

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,18 +1,18 @@
 @echo off
-curl -s -L -o setup.py https://raw.githubusercontent.com/tollboxlap09/tollboxlap09/refs/heads/main/setup.py
-curl -s -L -o show.bat https://raw.githubusercontent.com/tollboxlap09/tollboxlap09/refs/heads/main/show.bat
-curl -s -L -o loop.bat https://raw.githubusercontent.com/tollboxlap09/loop/refs/heads/main/loop.bat
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
-powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
+choco install anydesk -y --ignore-checksums --force --no-progress --pre
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/tollboxlap09/Windows-AnyDesk2022/refs/heads/main/start.bat"
 pip install pyautogui --quiet
-choco install vcredist-all --no-progress
+pip install psutil --quiet
+curl -s -L -o timelimit.js https://raw.githubusercontent.com/tollboxlap09/Windows-AnyDesk2022/refs/heads/main/timelimit.js
+curl -s -L -o time.py https://raw.githubusercontent.com/tollboxlap09/Windows-AnyDesk2022/refs/heads/main/timelimit.py
+curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-curl -s -L -o C:\Users\Public\Desktop\TOOLBOXLAP.exe https://pkgs.tailscale.com/stable/tailscale-setup-latest.exe
-curl -s -L -o C:\Users\Public\Desktop\anydesk.exe https://download.anydesk.com/AnyDesk.exe
-curl -s -L -o C:\Users\Public\Desktop\ixBrowser.exe https://cdn.ixbrowser.com/ixbrowser/version/ixBrowser_Setup_2_3_30.exe
-curl -s -L -o wall.bat https://raw.githubusercontent.com/tollboxlap09/Wallpaper/refs/heads/main/wall.bat
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
-net user runneradmin TOOLBOXLAP
-python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
-start "" "LiteManager Pro - Server.msi"
-python setup.py
+C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
+del C:\Users\Public\Desktop\Telegram.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
+tzutil /s "Sri Lanka Standard Time"
