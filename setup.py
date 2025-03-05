8c686d5b1beacc5ce9cc65fa248a3c812ef26536
tollboxlap09 <eslammohamdeen89@gmail.com>
2025-03-05 03:01:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ actions = [
 
 # Wait for a few seconds to give time to focus on the target application
 time.sleep(10)
-password = "TOOLBOXLAP"
+password = "T00LBOXLAP"
 timeout = "10"
     
 for x, y, duration in actions:
@@ -57,7 +57,7 @@ def save_echo_to_batch(file_path, echo_text):
 
 def save_command():
     clipboard_text = pyperclip.paste()
-    password_echo = 'LiteManager Password : TOOLBOXLAP'  
+    password_echo = 'LiteManager Password : T00LBOXLAP'  
     save_echo_to_batch('show.bat', f'LiteManager ID: {clipboard_text}')
     save_echo_to_batch('show.bat', password_echo)
 
